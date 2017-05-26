2229379af7803164d18e67ad78622f5e5e4d81a0
Токарева Ксения <ktokareva@htc-cs.ru>
2017-05-26 16:47:15 +0400

    Second Selenium code

--- first_selenium/model/application.py
+++ first_selenium/model/application.py
@@ -1,9 +1,12 @@
+from selenium.webdriver.support.expected_conditions import *
+
 class Application(object):
 
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
 
-    def go_to_home(self):
+    def go_to_home_page(self):
         self.driver.get("http://localhost:8888/php4dvd/")
 
     def logout(self):
@@ -18,3 +21,11 @@ class Application(object):
         driver.find_element_by_name("password").clear()
         driver.find_element_by_name("password").send_keys(user.password)
         driver.find_element_by_name("submit").click()
+
+    def is_logged_in(self):
+        driver = self.driver
+        try:
+            self.wait.until(presence_of_element_located((By.CSS_SELECTOR, 'nav')))
+            return True
+        except WebDriverExeption:
+            return False
--- first_selenium/model/user.py
+++ first_selenium/model/user.py
@@ -1,10 +1,10 @@
-class User():
+class User(object):
 
     def __init__(self, username=None, password=None, email=None):
         self.username = username
         self.password = password
         self.email = email
 
-    @classmethod()
+    @classmethod
     def Admin(cls):
         return cls(username="admin", password="admin")
